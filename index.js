564f22e2a933bb937b474d29178473672ca9b305
Josh <josh@joshbosworth.com>
2016-06-04 14:19:46 -0700

    finish server, add client directory

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-var foo = bar;
+import makeStore from './src/store';
+import {startServer} from './src/server';
+
+let entries = require('./data/entries.json');
+
+export const store = makeStore();
+startServer(store);
+
+store.dispatch({
+    type:'SET_ENTRIES',
+    entries : entries
+});
