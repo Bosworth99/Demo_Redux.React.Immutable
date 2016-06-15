8b2e96e61dd4b29102afb5510d84c35e1b418a03
Josh <josh@joshbosworth.com>
2016-06-15 12:52:29 -0700

    final app. no data. need to tweak

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import { connect } from 'react-redux';
+import Winner from './Winner';
+import * as actionCreators from '../action_creators';
 
-export default React.createClass({
+export const Results = React.createClass({
     displayName : 'Component::Results',
     mixins : [PureRenderMixin],
     getPair : function(){
-        console.log(`Results::getPair pair : ${this.props.pair}`);
         return this.props.pair || [];
     },
     getVotes : function(entry){
@@ -15,15 +17,38 @@ export default React.createClass({
         return 0;
     },
     render : function(){
-        return <div className="results">
-            {this.getPair().map( entry =>
-                <div key={entry} className="entry">
-                    <h1>{entry}</h1>
-                    <div className="voteCount">
-                        {this.getVotes(entry)}
-                    </div>
+        return this.props.winner ?
+            <Winner ref="winner" winner={this.props.winner} /> :
+            <div className="results">
+                <div className="tally">
+                    {this.getPair().map( entry =>
+                        <div key={entry} className="entry">
+                            <h1>{entry}</h1>
+                            <div className="voteCount">
+                                {this.getVotes(entry)}
+                            </div>
+                        </div>
+                    )}
                 </div>
-            )}
-        </div>;
+                <div className="management">
+                    <button ref="next"
+                            className="next"
+                            onClick={this.props.next}>
+                    </button>
+                </div>
+            </div>
     }
 });
+
+function mapStateToProps(state){
+    return {
+        pair : state.getIn(['vote', 'pair']),
+        tally : state.getIn(['vote', 'tally']),
+        winner : state.get('winner')
+    }
+}
+
+export const ResultsContainer = connect(
+    mapStateToProps,
+    actionCreators
+)(Results);
--- client/test/components/Results_spec.jsx
+++ client/test/components/Results_spec.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import {
   renderIntoDocument,
-  scryRenderedDOMComponentsWithClass
+  scryRenderedDOMComponentsWithClass,
+  Simulate
 } from 'react-addons-test-utils';
 import {List, Map} from 'immutable';
-import Results from '../../src/components/Results';
+import { Results } from '../../src/components/Results';
 import {expect} from 'chai';
 
 describe('Results', ()=>{
@@ -26,16 +28,28 @@ describe('Results', ()=>{
     });
 
     it('invokes the next callback when next button is clicked', ()=>{
+        let nextInvoked = false;
+        const next = () => nextInvoked = true;
+        const pair = List.of('Trainspotting', '28 Days Later');
+        const component = renderIntoDocument(
+            <Results    pair={pair}
+                        tally={Map()}
+                        next={next} />
+        );
 
-        //*********************************************************************
-        // YOU IS HERE
-        //
-        // search dis text
-        // > Next, let's talk about the "Next" button, which is used to move the voting to the next entry.
-        //*********************************************************************
-
-
-
+        Simulate.click(ReactDOM.findDOMNode(component.refs.next));
     });
+
+    it('renders the winner when there is one',  ()=>{
+        const component = renderIntoDocument(
+            <Results    winner="Trainspotting"
+                        pair={["Trainspotting", "28 Days Later"]}
+                        tally={Map()} />
+        );
+        const winner = ReactDOM.findDOMNode(component.refs.winner);
+        expect(winner).to.be.ok;
+        expect(winner.textContent).to.contain("Trainspotting");
+    });
+
 
 });
